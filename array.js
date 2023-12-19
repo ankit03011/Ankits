dffaced2673c515e3310c0fec2af497d16ae7c91
ankit <ankitmech2016@gmail.com>
2023-12-19 12:25:07 +0530

    shivam

---
+++ array.js
@@ -0,0 +1,57 @@
+// let array1 = ["a", "b", "c"];
+
+// let array2 =["d", "e", "f"];
+
+
+// let arrayConcat=array1.concat(array2);
+
+// console.log(arrayConcat);
+
+////////////////////////////////////
+
+// let array1 = ["a", "b", "c"];
+// console.log(array1)
+
+// array1[3]="d"
+
+// console.log(array1)
+
+/////////////////////
+
+// let array1 = ["a", "b", "c"];
+// console.table(array1)
+// console.log(array1.indexOf("b"))
+
+///////////////////
+// let student = ["ankit", "alok", "gautam"];
+
+// console.log(student.length);
+
+// let remove = student.pop()
+// console.log(remove, student)
+// console.log(student.length);
+
+
+
+
+
+
+// var value = 5
+
+// let student = ["ankit", "alok", "gautam"];
+
+// console.log(student.length)
+
+// let remove = student.pop()
+// console.log(remove, student)
+
+// console.log(student.length)
+
+
+let student = ["ankit", "alok", "gautam"];
+console.log(student.length)
+console.log(student)
+
+let add=student.push("amol")
+
+console.log(add,student)
